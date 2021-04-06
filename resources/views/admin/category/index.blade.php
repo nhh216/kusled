ce38bea2dd0070e0ba73339805e16074fe064e92
manh2911 <manhhoang291196@gmail.com>
2021-04-06 22:28:29 +0700

    category 1

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -4,7 +4,7 @@
         <!-- Content Header (Page header) -->
         <div class="content-header">
             <div class="container-fluid">
-                <div class="row mb-2">m
+                <div class="row mb-2">
                     <div class="col-sm-6">
                         <h1 class="m-0">Danh mục</h1>
                     </div><!-- /.col -->
