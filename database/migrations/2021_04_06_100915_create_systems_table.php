b1e1be2d0c52ac93e115ac6a975098977106fb6b
manh2911 <manhhoang291196@gmail.com>
2021-04-06 17:19:06 +0700

    migrate

---
+++ database/migrations/2021_04_06_100915_create_systems_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSystemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('systems', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->nullable();
+            $table->string('key')->nullable();
+            $table->string('value')->nullable();
+            $table->string('link')->nullable();
+            $table->text('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('systems');
+    }
+}
