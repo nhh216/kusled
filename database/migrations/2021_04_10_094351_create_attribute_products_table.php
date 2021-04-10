d14882e0638b3d182b80096f268dae311b134f7c
Huy Hung <nhh.216@gmail.com>
2021-04-10 21:05:17 +0700

    update

---
+++ database/migrations/2021_04_10_094351_create_attribute_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributeProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attribute_products', function (Blueprint $table) {
+            $table->integer('attribute_id');
+            $table->integer('product_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attribute_products');
+    }
+}
