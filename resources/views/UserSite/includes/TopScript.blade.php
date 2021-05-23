cd537b8703f9ae22cb758152da60284ceca168b1
Huy Hung <nhh.216@gmail.com>
2021-05-23 12:07:24 +0700

    update

--- resources/views/UserSite/includes/TopScript.blade.php
+++ resources/views/UserSite/includes/TopScript.blade.php
@@ -1,7 +1,6 @@
 @include('UserSite.includes.Meta')
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
 <link rel="stylesheet" id="lib-css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" as="style" onload="this.onload=null;" type="text/css" media="all">
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 {{--<link rel="stylesheet" id="carousel-css" href="{{ asset('css/owl.carousel.css') }}" as="style" onload="this.onload=null;" type="text/css" media="all">--}}
