fd3f1abe8741e1d453fdc3a149de345fb64eff8f
HungGimo <nhh.216@gmail.com>
2021-05-24 09:14:43 +0700

    update info in product detail

--- resources/views/UserSite/pages/post/Posts.blade.php
+++ resources/views/UserSite/pages/post/Posts.blade.php
@@ -12,192 +12,69 @@
                 <h3 class="modtitle">Danh mục tin</h3>
                 <div class="modcontent">
                     <ul class="list-group ">
-                        <li class="list-group-item"><a href="https://gtrvietnam.com/tin-tuc/thong-tin-gtr"
-                                                       class="group-item active">Thông tin GTR</a></li>
-                        <li class="list-group-item"><a href="https://gtrvietnam.com/tin-tuc/hieu-qua-chieu-sang"
-                                                       class="group-item active">Hiệu quả chiếu sáng</a></li>
-                        <li class="list-group-item"><a href="https://gtrvietnam.com/tin-tuc/huong-dan-lap-dat"
-                                                       class="group-item active">Hướng dẫn lắp đặt</a></li>
-                        <li class="list-group-item"><a href="https://gtrvietnam.com/tin-tuc/tin-tuc-do-xe-hoi"
-                                                       class="group-item active">Tin tức Độ Đèn</a></li>
-                        <li class="list-group-item"><a href="https://gtrvietnam.com/tin-tuc/hinh-anh-do-den"
-                                                       class="group-item active">Hình ảnh độ đèn</a></li>
+
                     </ul>
 
                 </div>
             </div>
-            <div class="module banner-left hidden-xs ">
-                <div class="banner-sidebar banners">
-                    <div>
-                        <a title="Auto365.vn" href="https://gtrvietnam.com/tin-tuc#">
-                            <img src="./Tin tức_files/banner-sidebar.jpg" alt="Auto365.vn">
-                        </a>
-                    </div>
-                </div>
-            </div>
+{{--            <div class="module banner-left hidden-xs ">--}}
+{{--                <div class="banner-sidebar banners">--}}
+{{--                    <div>--}}
+{{--                        <a title="Auto365.vn" href="https://gtrvietnam.com/tin-tuc#">--}}
+{{--                            <img src="./Tin tức_files/banner-sidebar.jpg" alt="Auto365.vn">--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </aside>
         <!--Left Part End -->
         <!--Middle Part Start-->
         <div id="content" class="col-md-9 col-sm-8">
             <div class="blog-category clearfix">
-                <h3 class="modtitle">
-                    <a href="https://gtrvietnam.com/tin-tuc/thong-tin-gtr"
-                       style="color: #000;font-weight: bold;font-size: 2.4rem;margin-bottom: 20px;">Thông tin GTR</a>
-                </h3>
+{{--                <h3 class="modtitle">--}}
+{{--                    <a href="/tin-tuc/thong-tin-kus"--}}
+{{--                       style="color: #000;font-weight: bold;font-size: 2.4rem;margin-bottom: 20px;">Thông tin GTR</a>--}}
+{{--                </h3>--}}
                 <div class="blog-listitem row"
                      style="padding-bottom: 30px;border-bottom: 2px solid red;margin-bottom: 30px;">
+{{--                    <div class="blog-item col-lg-3 col-md-3 col-sm-4 col-xs-6">--}}
+{{--                        <div class="blog-item-inner clearfix">--}}
+{{--                            <div class="itemBlogImg clearfix">--}}
+{{--                                <div class="article-image">--}}
+{{--                                    <div>--}}
+{{--                                        <a href="https://gtrvietnam.com/he-thong-do-xe-auto365-chinh-thuc-tiep-tuc-phan-phoi-san-pham-gtr-tai-thi-truong-viet-nam">--}}
+{{--                                            <img--}}
+{{--                                                src="./Tin tức_files/bi-xenon-gtr-premium__1__1934a31c902d4871b0d0a6d647bc4b86_2048x2048(1).webp"--}}
+{{--                                                alt="HỆ THỐNG ĐỘ XE AUTO365 CHÍNH THỨC TIẾP TỤC PHÂN PHỐI SẢN PHẨM GTR TẠI THỊ TRƯỜNG VIỆT NAM">--}}
+{{--                                        </a>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="article-date">--}}
+{{--                                        <div class="date">  <span class="article-date">--}}
 
-                    <div class="blog-item col-lg-3 col-md-3 col-sm-4 col-xs-6">
-                        <div class="blog-item-inner clearfix">
-                            <div class="itemBlogImg clearfix">
-                                <div class="article-image">
-                                    <div>
-                                        <a href="https://gtrvietnam.com/he-thong-do-xe-auto365-chinh-thuc-tiep-tuc-phan-phoi-san-pham-gtr-tai-thi-truong-viet-nam">
-                                            <img
-                                                src="./Tin tức_files/bi-xenon-gtr-premium__1__1934a31c902d4871b0d0a6d647bc4b86_2048x2048(1).webp"
-                                                alt="HỆ THỐNG ĐỘ XE AUTO365 CHÍNH THỨC TIẾP TỤC PHÂN PHỐI SẢN PHẨM GTR TẠI THỊ TRƯỜNG VIỆT NAM">
-                                        </a>
-                                    </div>
-                                    <div class="article-date">
-                                        <div class="date">  <span class="article-date">
-
-                                        </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="itemBlogContent clearfix ">
-                                <div class="blog-content">
-                                    <div class="article-title font-title">
-                                        <h4>
-                                            <a href="https://gtrvietnam.com/he-thong-do-xe-auto365-chinh-thuc-tiep-tuc-phan-phoi-san-pham-gtr-tai-thi-truong-viet-nam">HỆ
-                                                THỐNG ĐỘ XE AUTO365 CHÍNH THỨC TIẾP TỤC PHÂN PHỐI SẢN PHẨM GTR TẠI THỊ
-                                                TRƯỜNG VIỆT NAM</a></h4>
-                                    </div>
-                                    <div class="blog-meta"><span class="author"><i
-                                                class="fa fa-user"></i><span>Post by </span>Auto365</span>
-                                    </div>
-                                    <div class="readmore"><a class="btn-readmore font-title"
-                                                             href="https://gtrvietnam.com/he-thong-do-xe-auto365-chinh-thuc-tiep-tuc-phan-phoi-san-pham-gtr-tai-thi-truong-viet-nam"><i
-                                                class="fa fa-caret-right"></i>Xem thêm</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="blog-item col-lg-3 col-md-3 col-sm-4 col-xs-6">
-                        <div class="blog-item-inner clearfix">
-                            <div class="itemBlogImg clearfix">
-                                <div class="article-image">
-                                    <div>
-                                        <a href="https://gtrvietnam.com/nha-san-xuat-danh-tieng-gtr-ra-mat-dong-sieu-pham-kit-xenon-&amp;-ballast-gtr-tang-sang-150-plus-special-edition-se-cho-thi-truong-viet-nam">
-                                            <img
-                                                src="./Tin tức_files/553a290d27a8c2f69bb9_7e60fbf7ac6146debf721fde366881aa_2048x2048.jpg"
-                                                alt="NHÀ SẢN XUẤT DANH TIẾNG GTR RA MẮT DÒNG SIÊU PHẨM KIT XENON &amp; BALLAST GTR TĂNG SÁNG 150 PLUS SPECIAL EDITION (SE) CHO THỊ TRƯỜNG VIỆT NAM">
-                                        </a>
-                                    </div>
-                                    <div class="article-date">
-                                        <div class="date">  <span class="article-date">
-
-                                        </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="itemBlogContent clearfix ">
-                                <div class="blog-content">
-                                    <div class="article-title font-title">
-                                        <h4>
-                                            <a href="https://gtrvietnam.com/nha-san-xuat-danh-tieng-gtr-ra-mat-dong-sieu-pham-kit-xenon-&amp;-ballast-gtr-tang-sang-150-plus-special-edition-se-cho-thi-truong-viet-nam">NHÀ
-                                                SẢN XUẤT DANH TIẾNG GTR RA MẮT DÒNG SIÊU PHẨM KIT XENON &amp; BALLAST
-                                                GTR TĂNG SÁNG 150 PLUS SPECIAL EDITION (SE) CHO THỊ TRƯỜNG VIỆT NAM</a>
-                                        </h4>
-                                    </div>
-                                    <div class="blog-meta"><span class="author"><i
-                                                class="fa fa-user"></i><span>Post by </span>Auto365</span>
-                                    </div>
-                                    <div class="readmore"><a class="btn-readmore font-title"
-                                                             href="https://gtrvietnam.com/nha-san-xuat-danh-tieng-gtr-ra-mat-dong-sieu-pham-kit-xenon-&amp;-ballast-gtr-tang-sang-150-plus-special-edition-se-cho-thi-truong-viet-nam"><i
-                                                class="fa fa-caret-right"></i>Xem thêm</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="blog-item col-lg-3 col-md-3 col-sm-4 col-xs-6">
-                        <div class="blog-item-inner clearfix">
-                            <div class="itemBlogImg clearfix">
-                                <div class="article-image">
-                                    <div>
-                                        <a href="https://gtrvietnam.com/tong-hop-danh-sach-dai-ly-do-den-gtr-viet-nam-tren-toan-quoc">
-                                            <img
-                                                src="./Tin tức_files/bi-xenon-gtr-premium__1__1934a31c902d4871b0d0a6d647bc4b86_2048x2048.webp"
-                                                alt="TỔNG HỢP DANH SÁCH ĐẠI LÝ ĐỘ ĐÈN GTR VIỆT NAM TRÊN TOÀN QUỐC" ">
-                                        </a>
-                                    </div>
-                                    <div class="article-date">
-                                        <div class="date">  <span class="article-date">
-
-                                        </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="itemBlogContent clearfix ">
-                                <div class="blog-content">
-                                    <div class="article-title font-title">
-                                        <h4>
-                                            <a href="https://gtrvietnam.com/tong-hop-danh-sach-dai-ly-do-den-gtr-viet-nam-tren-toan-quoc">TỔNG
-                                                HỢP DANH SÁCH ĐẠI LÝ ĐỘ ĐÈN GTR VIỆT NAM TRÊN TOÀN QUỐC</a></h4>
-                                    </div>
-                                    <div class="blog-meta"><span class="author"><i
-                                                class="fa fa-user"></i><span>Post by </span>Auto365</span>
-                                    </div>
-                                    <div class="readmore"><a class="btn-readmore font-title"
-                                                             href="https://gtrvietnam.com/tong-hop-danh-sach-dai-ly-do-den-gtr-viet-nam-tren-toan-quoc"><i
-                                                class="fa fa-caret-right"></i>Xem thêm</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="blog-item col-lg-3 col-md-3 col-sm-4 col-xs-6">
-                        <div class="blog-item-inner clearfix">
-                            <div class="itemBlogImg clearfix">
-                                <div class="article-image">
-                                    <div>
-                                        <a href="https://gtrvietnam.com/san-pham-hot-nhat-trong-thang-gtr-g-led-x-gtr-gled-2019">
-                                            <img
-                                                src="./Tin tức_files/01dsc09111_01d49802096a46ada8a31177aa7fcd70_1024x1024.jpg"
-                                                alt="SẢN PHẨM HOT NHẤT TRONG THÁNG: GTR G-LED X (GTR GLED 2019)" ">
-                                        </a>
-                                    </div>
-                                    <div class="article-date">
-                                        <div class="date">  <span class="article-date">
-
-                                        </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="itemBlogContent clearfix ">
-                                <div class="blog-content">
-                                    <div class="article-title font-title">
-                                        <h4>
-                                            <a href="https://gtrvietnam.com/san-pham-hot-nhat-trong-thang-gtr-g-led-x-gtr-gled-2019">SẢN
-                                                PHẨM HOT NHẤT TRONG THÁNG: GTR G-LED X (GTR GLED 2019)</a></h4>
-                                    </div>
-                                    <div class="blog-meta"><span class="author"><i
-                                                class="fa fa-user"></i><span>Post by </span>Auto365</span>
-                                    </div>
-                                    <div class="readmore"><a class="btn-readmore font-title"
-                                                             href="https://gtrvietnam.com/san-pham-hot-nhat-trong-thang-gtr-g-led-x-gtr-gled-2019"><i
-                                                class="fa fa-caret-right"></i>Xem thêm</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+{{--                                        </span>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="itemBlogContent clearfix ">--}}
+{{--                                <div class="blog-content">--}}
+{{--                                    <div class="article-title font-title">--}}
+{{--                                        <h4>--}}
+{{--                                            <a href="https://gtrvietnam.com/he-thong-do-xe-auto365-chinh-thuc-tiep-tuc-phan-phoi-san-pham-gtr-tai-thi-truong-viet-nam">HỆ--}}
+{{--                                                THỐNG ĐỘ XE AUTO365 CHÍNH THỨC TIẾP TỤC PHÂN PHỐI SẢN PHẨM GTR TẠI THỊ--}}
+{{--                                                TRƯỜNG VIỆT NAM</a></h4>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="blog-meta"><span class="author"><i--}}
+{{--                                                class="fa fa-user"></i><span>Post by </span>Auto365</span>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="readmore"><a class="btn-readmore font-title"--}}
+{{--                                                             href="https://gtrvietnam.com/he-thong-do-xe-auto365-chinh-thuc-tiep-tuc-phan-phoi-san-pham-gtr-tai-thi-truong-viet-nam"><i--}}
+{{--                                                class="fa fa-caret-right"></i>Xem thêm</a>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
 
                 <h3 class="modtitle">
