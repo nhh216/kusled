8fd82bb350dcd00de60e10d5c31824c419ee21b1
Huy Hung <nhh.216@gmail.com>
2021-10-31 21:07:13 +0700

    Update code - Update meta

--- resources/views/UserSite/includes/Meta.blade.php
+++ resources/views/UserSite/includes/Meta.blade.php
@@ -8,10 +8,10 @@
 <meta name="theme-color" content="#000000">
 <meta name="title" content="Led siêu sáng - Bi Led - Bi Laser - Đèn trợ sáng KUS Việt Nam">
 <meta name="description"
-      content="Kus Việt Nam - nhà cung cấp các sản phẩm led siêu sáng/ bi led/ bi xenon/ g-led/ ballast/ led bar phá sương - đi mưa chất lượng">
+      content="Nhà phân phối chính hãng các sản phẩm bi led và laser thương hiệu Kus tại Việt Nam">
 <meta name="keywords" content="Led siêu sáng - Bi Led - Bi Laser - Đèn trợ sáng KUS Việt Nam">
 {{--<meta name="image" content="http://kusvietnam.com/upload/images/Garage-uy-tin-hien-dai-auto365-1.png?v=1.1.1">--}}
-<meta name="generator" content="Công Ty Thiết Kế Web, Phần mềm STACKGOO">
+<meta name="generator" content="Công Ty Thiết Kế HHW">
 <meta name="robots" content="index,follow">
 <meta name="author" content="thương hiệu">
 <meta name="refresh" http-equiv="refresh"/>
@@ -21,7 +21,8 @@
 <meta property="og:locale" content="vi_VN">
 <meta property="og:url" content="https://nothing.com">
 <meta property="og:type" content="website">
-<meta property="og:description" content="Kus Việt Nam - nhà cung cấp các sản phẩm led siêu sáng - bi led - bi xenon -led bar phá sương đi mưa chất lượng">
+<meta property="og:description"
+      content="Nhà phân phối chính hãng các sản phẩm bi led và laser thương hiệu Kus tại Việt Nam">
 @if($homepage)
     <meta property="og:title" content="Led siêu sáng - Bi Led - Bi Laser - Đèn trợ sáng KUS Việt Nam">
     <meta property="og:image" content="{{ asset($favicon -> link) }}">
@@ -32,4 +33,6 @@
 <meta property="og:image:height" content="560">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 {{--<link rel="shortcut icon" type="image/png" href="http://kusvietnam.com/upload/images/default/favicon-16x16.png">--}}
-<link rel="stylesheet" id="googleapis-css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,600,700" as="style" type="text/css" media="all">
+<link rel="stylesheet" id="googleapis-css"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,600,700" as="style" type="text/css"
+      media="all">
