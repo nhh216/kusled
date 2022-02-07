e81ff4e454b2bd0251e49dfa6aed14ed3c33df8d
nhh216 <nhh.216@gmailcom>
2022-02-07 20:20:50 +0700

    update code - Get 6 post newest in home page

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -7,7 +7,7 @@ use App\Models\Post;
 class HomePageController extends Controller
 {
     public function index() {
-        $posts = Post::all();
+        $posts = Post::orderBy('updated_at', 'desc')->paginate(6);
         return view('./UserSite/HomePage/HomePage')->with('posts', $posts);
     }
 }
