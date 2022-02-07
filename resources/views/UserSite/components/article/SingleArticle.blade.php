c18539b787d50dc24b72789f56112429ca8959e1
nhh216 <nhh.216@gmailcom>
2022-02-07 18:17:29 +0700

    update code - Fix picture in post - HomePage

--- resources/views/UserSite/components/article/SingleArticle.blade.php
+++ resources/views/UserSite/components/article/SingleArticle.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     @foreach($posts as $post)
         <div class=" col-lg-2 col-md-3 col-sm-6 col-xs-6" style="max-height: 300px">
-            <div class="blog-img img-hover_effect" style="height: 125px">
+            <div class="blog-img img-hover_effect" style="width: 223px; height: 167px">
                 <a href="tin-tuc/{{ $post->slug }}_{{ $post->id }}"
                    tabindex="0">
                     <img src="{{ asset($post->image) }}"
